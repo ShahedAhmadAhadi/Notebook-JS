ab211de91280edfa63885e95bc02d42efc9b4a0d
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-10 22:56:58 +0430

    esbuild wasm added to public and used for transform of our string in state to js code

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,45 @@
-import { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const [input, setInput] = useState('')
-  const [code, setCode] = useState('')
+    const ref = useRef<any>();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm',
+        });
+    };
 
-  const clickHandler = () => {
-    console.log(input)
-  }
+    useEffect(() => {
+        startService();
+    }, []);
 
-    return <div>
-      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
-      <div>
-        <button onClick={clickHandler}>Submit</button>
-      </div>
-      <pre>{code}</pre>
-    </div>
+    const clickHandler = async () => {
+        if (!ref.current) {
+            return;
+        }
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015',
+        });
+        console.log(result)
+    };
+
+    return (
+        <div>
+            <textarea
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            ></textarea>
+            <div>
+                <button onClick={clickHandler}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
