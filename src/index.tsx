c4cafe3165db4340ea197660bc77d1bb3a095738
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-10 22:39:43 +0430

    project core setup tested and exact package added

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+
+  const clickHandler = () => {
+    console.log(input)
+  }
+
+    return <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={clickHandler}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
