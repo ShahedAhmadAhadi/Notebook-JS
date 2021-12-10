a4aa835ed73ee04c36a98f8c57c875decd87e2ba
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-11 00:35:28 +0430

    every type of path added by user will be downloaded from unpkg and even inside of it relative path configured and success!

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,23 +1,30 @@
-import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
+        console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
-          
         }
-        if (args.path.includes('./') || args.paths.includes('../')) {
-          return { path: new URL(args.path, args.importer + '/').href, namespace: 'a'}
-          
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
+          };
         }
-        // else if (args.path === 'tiny-test-pkg') {
-        //   return { path: `https://unpkg.com/tiny-test-pkg@1.0.0/index.js`, namespace: 'a'}
-        // }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -27,17 +34,20 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
-              console.log(message);
+              import React, { useState } from 'react';
+              console.log(React, useState);
             `,
           };
         }
-        const { data } = await axios.get(args.path)
+
+        const { data, request } = await axios.get(args.path);
         return {
           loader: 'jsx',
-          contents: data
-        }
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
       });
     },
   };
 };
+
